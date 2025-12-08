3f6c949ed60bc78e9d7bd94b1538acc6e6fef5ff
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-08 18:50:45 +0530

    controlflow.js pratice

---
+++ controlflow.js
@@ -0,0 +1,21 @@
+{
+  let i;
+  for (i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+{
+  let i = 11;
+  while (i <= 20) {
+    console.log(i);
+    i++;
+  }
+}
+{
+  let i = 21;
+  do {
+    console.log(i);
+    i++;
+  } while (i <= 30);
+}
+log
