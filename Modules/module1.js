7031a0fc2ee7bdc1b4bf2310d44c27825e358abd
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-17 08:04:14 +0530

    add modules.js

---
+++ Modules/module1.js
@@ -0,0 +1,10 @@
+export let num1 = 100;
+export var num2 = 200;
+export const num3 = "instializing.....";
+
+export default function agadesh() {
+  let i;
+  for (i = 10; i > 0; i--) {
+    console.log(i);
+  }
+}
---
+++ Modules/module2.js
@@ -0,0 +1,6 @@
+import agadesh from "./module1.js";
+import { num1 as aga, num2, num3 } from "./module1.js";
+
+agadesh();
+console.log(aga);
+console.log(num2, num3);
