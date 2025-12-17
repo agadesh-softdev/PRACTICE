8101c740d2b600a0c358794b9bac620a6656d02f
agadesh-softdev <agadesh.softdev@gmail.com>
2025-12-17 07:51:47 +0530

    modify controlflow.js

--- controlflow.js
+++ controlflow.js
@@ -18,3 +18,23 @@
     i++;
   } while (i <= 30);
 }
+
+{
+  for (i = 10; i > 0; i--) {
+    console.log(i);
+  }
+}
+{
+  let i = 10;
+  while (i > 0) {
+    console.log(i);
+    i--;
+  }
+}
+{
+  i = 10;
+  do {
+    console.log(i);
+    i--;
+  } while (i > 0);
+}
